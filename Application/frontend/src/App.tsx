f23c511ad1179c0ef8544c3921981474ceb9407a
dusanmaksimovic02 <116552456+dusanmaksimovic02@users.noreply.github.com>
2024-12-25 18:25:54 +0100

    Project added

---
+++ Application/frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-extrabold">Proba</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ Application/frontend/src/vite-env.d.ts
@@ -0,0 +1,22 @@
+/// <reference types="vite/client" />
+declare module "*.jpg" {
+    const value: string;
+    export default value;
+  }
+  declare module "*.jpeg" {
+    const value: string;
+    export default value;
+  }
+  declare module "*.png" {
+    const value: string;
+    export default value;
+  }
+  declare module "*.ico" {
+    const value: string;
+    export default value;
+  }
+  declare module "*.gif" {
+    const value: string;
+    export default value;
+  }
+  
