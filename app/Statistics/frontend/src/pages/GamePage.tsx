a6a01c18cf770a7e232ade28253587ca72902988
dusanmaksimovic02 <116552456+dusanmaksimovic02@users.noreply.github.com>
2026-01-17 21:02:01 +0100

    Game page updated

--- app/Statistics/frontend/src/pages/GamePage.tsx
+++ app/Statistics/frontend/src/pages/GamePage.tsx
@@ -1,12 +1,17 @@
+import TeamTable from "../components/GamePage/TeamTable";
 import ActivePlayers from "../components/GamePage/ActivePlayers";
 import GameInfo from "../components/GamePage/GameInfo";
 import type { FC } from "react";
+import { useParams } from "react-router-dom";
 
 const GamePage: FC = () => {
+  const { team1, team2 } = useParams();
   return (
     <div className="w-screen h-fit min-h-screen pt-15">
       <GameInfo />
       <ActivePlayers />
+      <TeamTable team={team1!} />
+      <TeamTable team={team2!} />
     </div>
   );
 };
